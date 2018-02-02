4fb9c6932631dadcc14e315ff041a180a92e1137
Andrii <andrii@Andriis-MacBook-Pro.local>
2018-02-02 14:31:09 +0200

    fix date field

--- tasks/forms.py
+++ tasks/forms.py
@@ -2,6 +2,10 @@ from django import forms
 from .models import Task
 
 
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+
 class TaskForm(forms.ModelForm):
     """
     A model form for a Task object.
@@ -9,3 +13,6 @@ class TaskForm(forms.ModelForm):
     class Meta:
         exclude = ['pk', 'owner', 'complete_time']
         model = Task
+        widgets = {
+            'due_date': DateInput()
+        }
